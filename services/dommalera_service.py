804a3896bbf61b3a0632313cb651e19aaa29d8b5
999cmp <999cmp@mail.ru>
2024-12-11 22:10:54 +0300

    fix

--- services/dommalera_service.py
+++ services/dommalera_service.py
@@ -2,6 +2,7 @@ import requests
 
 def send_sms_to_dommalera(phone_number: str):
     try:
+        print(f'Domalera: {phone_number}')
         url = f'https://www.dommalera.ru/ajax/sms_login.php?&phoneauth={phone_number}'
         headers = {
             "Content-Type": "application/x-www-form-urlencoded",
