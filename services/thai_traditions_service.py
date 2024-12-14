4e16ec172e187c1f9045cf76813d8988f5a55943
999cmp <999cmp@mail.ru>
2024-12-14 06:53:50 +0300

    update TT

--- services/thai_traditions_service.py
+++ services/thai_traditions_service.py
@@ -10,6 +10,7 @@ def send_sms_to_thai_traditions(phone_number: str):
             "TYPE": "AUTH",
             "backurl": "",
             "USER_TEL": phone_number,
+            "USER_FIO_NAME": "",
             "USER_TEL_CODE": ""
         }
 
@@ -18,7 +19,12 @@ def send_sms_to_thai_traditions(phone_number: str):
             "https": Proxy.PROXY_URL
         }
 
-        response = requests.post(url, data=payload, proxies=proxies)
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+        }
+
+        response = requests.post(url, data=payload, headers=headers, proxies=proxies)
 
         return {"status_code": response.status_code, "response": response}
     except Exception as e:
