075619693bf2578cca9005be3ee02efea2387ad8
Daniel Egozaryan <dan09ki@mail.ru>
2025-02-05 07:50:53 +0300

    fix

--- services/pm_ru_service.py
+++ services/pm_ru_service.py
@@ -15,14 +15,13 @@ def send_sms_to_pm_ru(phone_number: str):
     captcha = main(url='https://pm.ru', site_key='6Ld29rUqAAAAAGrJgIJiBpDnt35tVhIn-2mE9tF0')
 
     # Удаляем пробелы и скобки из номера телефона
-    clean_phone_number = phone_number.replace(" ", "").replace("(", "").replace(")", "")
 
     data = {
         "g-recaptcha-response": captcha,
-        "lk-phone": clean_phone_number,
+        "lk-phone": phone_number,
         "users-agreement": "on",
         "lk-code": "",
-        "phone": clean_phone_number
+        "phone": phone_number
     }
 
     proxies = {
@@ -34,13 +33,12 @@ def send_sms_to_pm_ru(phone_number: str):
         "accept": "application/json",
         "accept-encoding": "gzip, deflate, br, zstd",
         "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-        "content-type": "application/json",
+        "content-type": "application/json",  # Измените на application/json если это требуется API
         "origin": "https://pm.ru",
         "referer": "https://pm.ru/",
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
     }
 
-    response = session.post(url, json=data, headers=headers, proxies=proxies)
-    print(f'PM_RU: {response.status_code}, {response.text}')
+    response = session.post(url, json=data, headers=headers, proxies=proxies)  # Обратите внимание на использование json=data
 
-    return {"status_code": response.status_code, "response": 'good'}
+    return {"status_code": response.status_code, "response": response.json()}
