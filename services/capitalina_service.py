65da21df4774d331dd2cba3d68a45cb93460ccd9
Daniel Egozaryan <dan09ki@mail.ru>
2025-03-06 08:37:42 +0300

    fix

--- services/capitalina_service.py
+++ services/capitalina_service.py
@@ -1,42 +1,58 @@
 import requests
-import uuid
 from config import Proxy, Services
-from utils.response_utils import get_cookies_and_headers
+from utils.email_generate import generate_random_string
+import json
+import time
+import random
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
 
 def send_sms_to_capitalina(phone_number: str, proxy: str = Proxy.PROXY_URL):
+    # Установка драйвера Chrome
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
     try:
-        url = 'https://api.finters-zaem.ru/anketa/api/send-sms/'
-        headers = {
-            'accept': '*/*',
-            'accept-encoding': 'gzip, deflate, br, zstd',
-            'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-            'content-length': '30',
-            'content-type': 'application/json',
-            'origin': 'https://finters-zaem.ru',
-            'cookie': '_ga=GA1.1.1057073382.1741235903; session_id=df46718a-3bef-4225-bfb9-7704d3ce8bfb; track_id=420878462; webmaster_id=18954; affiliate_id=bankiru; _ym_uid=1741235903622393947; _ym_d=1741235903; _ym_isad=1; _ym_visorc=w; _ga_W7TK0EQZ03=GS1.1.1741235902.1.1.1741235918.0.0.0',
-            'priority': 'u=1, i',
-            'referer': 'https://finters-zaem.ru/',
-            'sec-ch-ua': '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-site',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
-            'x-request-id': str(uuid.uuid4())
-        }
-        phone_number = f"+7 ({phone_number[2:5]}) {phone_number[5:8]}-{phone_number[8:10]}-{phone_number[10:]}"
-        payload = {
-        "phone": phone_number
-        }
+        # Открываем нужный URL
+        url = 'https://finters-zaem.ru/anketa'
+        driver.get(url)
+        wait = WebDriverWait(driver, 10)
+        last_n = wait.until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="lastname"]')))
+        first_n = wait.until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="firstname"]')))
+        surname = wait.until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="middlename"]')))
+        phone = wait.until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="phone"]')))
+        phone.click()
+        phone.send_keys(phone_number[2])
+        phone.send_keys(phone_number[3])
+        phone.send_keys(phone_number[4])
+        phone.send_keys(phone_number[5])
+        phone.send_keys(phone_number[6])
+        phone.send_keys(phone_number[7])
+        phone.send_keys(phone_number[8])
+        phone.send_keys(phone_number[9])
+        phone.send_keys(phone_number[10])
+        phone.send_keys(phone_number[10])
+        last_n.send_keys('Михаилович')
+        surname.send_keys('Прохоров')
+        first_n.send_keys('Андрей')
+        checkbox1 = wait.until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="accept-personal-data"]')))
+        checkbox1.click()
+        phone.submit()
 
-        proxies = {
-            "http": proxy,
-            "https": proxy
-        }
-        session = requests.session()
-
-        response = session.post(url, json=payload, proxies=proxies, headers=headers)
-        return {"status_code": response.status_code, "response": response.text}
-    except Exception as e:
-        print(e)
+        time.sleep(8)
+    except Exception as error:
+        print(type(error), error)
+    finally:
+        driver.quit()
+        return {"status_code": 200, "response": 'гуд'}
