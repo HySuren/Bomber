6196a191dd8b51f0f9e74456e092ed1265f65c90
Daniel Egozaryan <dan09ki@mail.ru>
2025-03-12 18:30:35 +0300

    fix

--- app.py
+++ app.py
@@ -230,7 +230,8 @@ class SmsServiceThread(threading.Thread):
         return get_sms_per_min(service_name)
 
     def run(self):
-        time_since_last_sent = time.time() - self.last_sent_timestamp
+        now = time.time()
+        time_since_last_sent = now - self.last_sent_timestamp
         time_to_wait = (60 / self.sms_per_min) - time_since_last_sent
         while not self.stop_event.is_set():
             try:
